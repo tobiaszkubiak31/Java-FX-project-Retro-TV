8f71190950ead66fe35ce0c45c35ec26da07c3ef
ukasz <zalewski.ukas@gmail.com>
2019-11-07 16:41:09 +0100

    added lights and disco mode

--- src/pipboy/Controller.java
+++ src/pipboy/Controller.java
@@ -2,11 +2,14 @@ package pipboy;
 
 import javafx.animation.*;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
+
+import static javafx.animation.Animation.INDEFINITE;
 
 
 public class Controller {
@@ -15,8 +18,11 @@ public class Controller {
 
     public ToggleButton powerButton;
 
-    public ImageView pointer;
+    public ImageView geigerPointer;
 
+    public ImageView lights;
+
+    // jak to przeniesc do cssa???
     private String powerOFF =
             "-fx-background-radius: 100em;\n" +
             "-fx-min-width: 62px;\n" +
@@ -31,8 +37,15 @@ public class Controller {
             "-fx-focus-color: transparent;\n" +
             "-fx-background-image: url('/pipboy/img/powerON.png');";
 
-    public void initialize(){
 
+    public void initialize(){
+        initBackground();
+        initPowerButton();
+        initGeigerPointer();
+        initLights();
+    }
+
+    private void initBackground(){
         BackgroundImage pipBoy = new BackgroundImage(
                 new Image("pipboy/img/pipboy.png"),                 // image
                 BackgroundRepeat.NO_REPEAT,                             // repeatX
@@ -48,45 +61,92 @@ public class Controller {
                 )
         );
         anchorPane.setBackground(new Background(pipBoy));
+    }
 
-
-        //POWER BUTTON
+    private void initPowerButton(){
         powerButton.setTranslateX(155);
         powerButton.setTranslateY(540);
         powerButton.setStyle(powerOFF);
         powerButton.setPickOnBounds(false);
+    }
 
-
-        //GEIGER COUNTER
-        pointer.setImage(new Image("/pipboy/img/geigerPointer.png"));
-        pointer.setTranslateX(843);
-        pointer.setTranslateY(142);
-        pointer.getTransforms().add(new Rotate(-70.0, 0, 0));
+    private void initGeigerPointer(){
+        geigerPointer.setImage(new Image("/pipboy/img/geigerPointer.png"));
+        geigerPointer.setTranslateX(843);
+        geigerPointer.setTranslateY(142);
+        geigerPointer.getTransforms().add(new Rotate(-70.0, 0, 0));
 
         Rotate pointerRotation = new Rotate(0.0, 0, 0);
-        pointer.getTransforms().add(pointerRotation);
+        geigerPointer.getTransforms().add(pointerRotation);
 
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.ZERO, new KeyValue(pointerRotation.angleProperty(), 0)),
                 new KeyFrame(Duration.seconds(1), new KeyValue(pointerRotation.angleProperty(), 100))
         );
 
-        timeline.cycleCountProperty().setValue(1000);   //loop 1000 times
+        timeline.cycleCountProperty().setValue(INDEFINITE);   //loop animation
         timeline.autoReverseProperty().setValue(true);
         timeline.play();
     }
 
 
-    public void onClick() {
+    private void initLights(){
+        lights.setFitWidth(-1); // -1 so it doesn't fit size to the parent
+        lights.setFitHeight(-1);
+        lights.setImage(new Image("/pipboy/img/lights.png"));
+        lights.setPreserveRatio(true);
+        lights.setTranslateX(791);
+        lights.setTranslateY(215);
+        ColorAdjust ca = new ColorAdjust();
+        ca.setSaturation(-1.0); //turn off lights
+        lights.setEffect(ca);
+    }
+
+    private void turnOnLights(){
+        ColorAdjust ca = (ColorAdjust)lights.getEffect();
+        double animTime = Math.abs(ca.getSaturation()) + 0.1;
+        Timeline t = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(ca.saturationProperty(), ca.getSaturation())),
+                new KeyFrame(Duration.seconds(animTime), new KeyValue(ca.saturationProperty(), 0.1))
+        );
+        t.play();
+    }
+
+    private void turnOffLights(){
+        ColorAdjust ca = (ColorAdjust)lights.getEffect();
+        double animTime = Math.abs(ca.getSaturation()) + 1.0;
+        Timeline t = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(ca.saturationProperty(), ca.getSaturation())),
+                new KeyFrame(Duration.seconds(animTime), new KeyValue(ca.saturationProperty(), -1.0))
+        );
+        t.play();
+    }
+
+    private void discoMode(){
+        ColorAdjust ca = new ColorAdjust();
+        lights.setEffect(ca);
+        Timeline t = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(ca.hueProperty(), -1.0)),
+                new KeyFrame(Duration.seconds(4), new KeyValue(ca.hueProperty(), 1.0))
+        );
+        t.cycleCountProperty().setValue(INDEFINITE);    //loop animation
+        t.autoReverseProperty().setValue(true);         //loop animation
+        t.play();
+    }
+
+    public void powerButtonOnClick() {
         if(powerButton.isSelected()){
             powerButton.setStyle(powerON);
+//            turnOnLights();
+            discoMode();
+
             System.out.println("POWER ON!");
         }
         else {
             powerButton.setStyle(powerOFF);
+            turnOffLights();
             System.out.println("POWER OFF!");
         }
-
     }
 
 }
